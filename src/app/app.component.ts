65cfb201943784e93ab339a5c5a0fa322a9f6b67
artiumangpatel <aartiumangpatel@gamil.com>
2022-06-09 09:57:46 +0530

    data binding & view encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'serverbinding';
+  newServerName='';
+  newServerContent='';
+
+
+
+
+  serverElements=[{type:'server',name:'TestServer',content:'it just a test!'}];
+
+  onServerAdded(serverData:{serverName:string,serverContent:string}){
+    this.serverElements.push({
+      type:'server',
+      name:serverData.serverName,
+      content:serverData.serverContent
+    });
+  }
+  onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}){
+    this.serverElements.push({
+      type:'blueprint',
+      name:blueprintData.serverName,
+      content:blueprintData.serverContent
+    });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerComponent } from './server/server.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
